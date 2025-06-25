bd77c63c79dc034d3c8f641e7b0fc50fcd65298d
benkeene <benkeene@pm.me>
2025-06-25 10:58:18 -0400

    start interact

--- 01_start_session.sh
+++ 01_start_session.sh
@@ -15,3 +15,18 @@ identify_cluster() {
 
 CLUSTER=$(identify_cluster)
 echo $CLUSTER
+
+case $CLUSTER in
+	"anvil.rcac.purdue.edu")
+		srun --account=cis240473 --partition=shared --time=01:00:00 --cpus-per-task=4 --job-name="testing" --pty bash
+		;;
+	"ib.bridges2.psc.edu")
+		interact -A cis240124p -p RM-shared -t 1:00:00 -n 4
+		;;
+	"expanse.sdsc.edu")
+		srun --partition=shared --pty --account=ucf122 --nodes=1 --ntasks-per-node=4 --time=01:00:00 bash
+		;;
+	*)
+		echo "ALERT: Cluster not found"
+		;;
+esac
