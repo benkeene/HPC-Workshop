5240c8d72dc611f6d992c6eddeb204f04fe25546
benkeene <benkeene@pm.me>
2025-06-25 13:37:45 -0400

    complete env purdue psc and sdsc

--- 02_python_env.sh
+++ 02_python_env.sh
@@ -9,17 +9,28 @@ case $CLUSTER in
 		export PIP_CACHE_DIR=/anvil/projects/x-cis240473/.pip/cache
 		conda create --prefix /anvil/projects/x-cis240473/envs/workshop python=3.11 pip -y
 		conda activate /anvil/projects/x-cis240473/envs/workshop
+		pip install ipykernel
+		python -m ipykernel install --prefix=/anvil/projects/x-cis240473/envs/workshop --display-name="Python (workshop)"
                 ;;
         "ib.bridges2.psc.edu")
 		module load anaconda3
 		conda init bash
 		export CONDA_PKGS_DIR=/ocean/projects/cis240124p/$(whoami)/.conda/packages
-		export PIP_CACHE_DIR=/ocean/projects/cis240124p/$(whoami)/.conda/packages
+		export PIP_CACHE_DIR=/ocean/projects/cis240124p/$(whoami)/.pip/cache
 		conda create --prefix /ocean/projects/cis240124p/$(whoami)/envs/workshop python=3.11 pip -y
 		conda activate /ocean/projects/cis240124p/$(whoami)/envs/workshop
+		pip install ipykernel
+		python -m ipykernel install --prefix=/ocean/projects/cis240124p/$(whoami)/envs/workshop --display-name="Python (workshop)"
                 ;;
         "expanse.sdsc.edu")
-
+		module purge
+		module load slurm sdsc cpu/0.15.4 gcc/10.2.0 anaconda3
+		export CONDA_PKGS_DIR=/expanse/lustre/projects/ucf122/$(whoami)/.conda/packages/
+		export PIP_CACHE_DIR=/expanse/lustre/projects/ucf122/$(whoami)/.pip/cache
+		conda create --prefix /expanse/lustre/projects/ucf122/$(whoami)/envs/workshop python=3.11 pip -y
+		conda activate /expanse/lustre/projects/ucf122/$(whoami)/envs/workshop
+		pip install ipykernel
+		python -m ipykernel install --prefix=/expanse/lustre/projects/ucf122/$(whoami)/envs/workshop --display-name="Python (workshop)"
                 ;;
         *)
                 echo "ALERT: Cluster not found"
