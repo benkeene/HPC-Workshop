12818e4a55f70e3a660e5ca96c6cbf521032b60d
benkeene <benkeene@pm.me>
2025-06-25 14:06:49 -0400

    module setup

--- activate.sh
+++ activate.sh
@@ -3,12 +3,16 @@
 CLUSTER=$(hostname -d)
 case $CLUSTER in
 	"anvil.rcac.purdue.edu")
+		module load anaconda
 		conda activate /anvil/projects/x-cis240473/envs/workshop
 		;;
 	"ib.bridges2.psc.edu")
+		module load anaconda3
 		conda activate /ocean/projects/cis240124p/$(whoami)/envs/workshop
 		;;
 	"expanse.sdsc.edu")
+		module purge
+		module load slurm sdsc cpu/0.15.4 gcc/10.2.0 anaconda3
 		conda activate /expanse/lustre/projects/ucf122/$(whoami)/envs/workshop
 		;;
 	*)
