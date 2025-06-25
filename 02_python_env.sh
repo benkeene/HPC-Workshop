b0a1124667f9339af27886042b471e30396965f0
benkeene <benkeene@pm.me>
2025-06-25 11:34:45 -0400

    explicit project dir

--- 02_python_env.sh
+++ 02_python_env.sh
@@ -4,10 +4,10 @@ case $CLUSTER in
         "anvil.rcac.purdue.edu")
 		module load anaconda
 		conda init bash
-		export CONDA_PKGS_DIRS=$PROJECT/.conda/packages/
-		export PIP_CACHE_DIR=$PROJECT/.pip/cache
-		conda create --prefix $PROJECT/envs/workshop python=3.11 pip -y
-		conda activate $PROJECT/envs/workshop
+		export CONDA_PKGS_DIRS=/anvil/projects/x-cis240473/.conda/packages/
+		export PIP_CACHE_DIR=/anvil/projects/x-cis240473/.pip/cache
+		conda create --prefix /anvil/projects/x-cis240473/envs/workshop python=3.11 pip -y
+		conda activate /anvil/projects/x-cis240473/envs/workshop
                 ;;
         "ib.bridges2.psc.edu")
 
